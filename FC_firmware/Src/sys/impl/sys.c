48c2b4c75528b4464a707428a84d37dd2d75309c
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2025-04-30 18:25:00 +0200

    Update sys

--- FC_firmware/Src/sys/impl/sys.c
+++ FC_firmware/Src/sys/impl/sys.c
@@ -1,21 +1,38 @@
 #include "sys/sys.h"
 #include "log/log.h"
 
-void init_hardware(sys_System* sys) {
+#include "act/act.h"
+#include "ctrl/ctrl.h"
+#include "dsp/dsp.h"
+#include "guide/guide.h"
+#include "llc/llc.h"
+#include "tel/tel.h"
+
+static gps_Gps _sys_Gps;
+static mag_Mag _sys_Mag;
+static acc_Acc _sys_Acc;
+static imu_Imu _sys_Imu;
+static bar_Bar _sys_Bar;
+static lora_Lora _sys_Lora;
+static esc_Esc _sys_Esc;
+static rc_Rc _sys_Rc;
+static sd_Sd _sys_Sd;
+
+void init_hardware(void) {
     log_Debug("Initalizing hardware...");
 
-    lora_Init();
-    sd_Init();
-    imu_Init();
-    rc_Init();
-    gps_Init();
-    mag_Init();
-    bar_Init();
-    esc_Init();
-    acc_Init();
+    lora_Init(&_sys_Lora);
+    sd_Init(&_sys_Sd);
+    imu_Init(&_sys_Imu);
+    rc_Init(&_sys_Rc);
+    gps_Init(&_sys_Gps);
+    mag_Init(&_sys_Mag);
+    bar_Init(&_sys_Bar);
+    esc_Init(&_sys_Esc);
+    acc_Init(&_sys_Acc);
 }
 
-void init_modules(sys_System* sys) {
+void init_modules(void) {
     log_Debug("Initalizing software modules...");
 
     tel_Init();
@@ -26,13 +43,13 @@ void init_modules(sys_System* sys) {
     dsp_Init();
 }
 
-void init(sys_System* sys) {
+void init(void) {
     log_Debug("Initalizing...");
 
-    init_hardware(sys);
-    init_modules(sys);
+    init_hardware();
+    init_modules();
 }
 
 void sys_Entry(void) {
-    init(&sys_Instance);
+    init();
 }
--- FC_firmware/Src/sys/sys.h
+++ FC_firmware/Src/sys/sys.h
@@ -9,33 +9,7 @@
 #include "lora/lora.h"
 #include "mag/mag.h"
 #include "rc/rc.h"
-
-#include "act/act.h"
-#include "ctrl/ctrl.h"
-#include "dsp/dsp.h"
-#include "guide/guide.h"
-#include "llc/llc.h"
-#include "tel/tel.h"
-
-typedef struct sys_System {
-    gps_Gps gps;
-    mag_Mag mag;
-    acc_Acc acc;
-    imu_Imu imu;
-    bar_Bar bar;
-    lora_Lora lora;
-    esc_Esc esc;
-    rc_Rc rc;
-
-    ctrl_Ctrl ctrl;
-    dsp_Dsp dsp;
-    act_Act act;
-    llc_Llc llc;
-    tel_Tel tel;
-    guide_Guide guide;
-} sys_System;
-
-sys_System sys_Instance;
+#include "sd/sd.h"
 
 void sys_Entry(void);
 

9d0c9dcde511c53a6b34d7e1bf05aa9f4da7c7bc
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2025-04-30 21:47:56 +0200

    Refactor act

--- FC_firmware/Src/act/act.h
+++ FC_firmware/Src/act/act.h
@@ -1,24 +1,18 @@
 #ifndef ACT_H
 #define ACT_H
 
+#include <stdint.h>
 #include "tim.h"
 
-#define PWM_min 1000    //timerek counter period 20000
-#define PWM_range 1000
+#define act_MOTOR_COUNT 4
 
+#define act_PWM_MIN 1000
+#define act_PWM_MAX 2000
 
-typedef struct {
-    TIM_HandleTypeDef* timer;
-    uint32_t channel;
-    uint8_t number;
-} act_Motor;
+#define act_PWM_RANGE (act_PWM_MAX - act_PWM_MIN)
 
+void act_Init(TIM_HandleTypeDef* timers[act_MOTOR_COUNT], uint32_t* channels[act_MOTOR_COUNT]);
 
-void act_Init(TIM_HandleTypeDef * tim1,uint32_t ch1,TIM_HandleTypeDef * tim2,uint32_t ch2,TIM_HandleTypeDef * tim3,uint32_t ch3,TIM_HandleTypeDef * tim4,uint32_t ch4);
-void act_CreateMotor(act_Motor* motor,TIM_HandleTypeDef * timer,uint32_t channel,uint8_t number);
-void act_PwmInit(TIM_HandleTypeDef * timer, uint32_t channel);
-void act_SetMotorSpeed( uint8_t number ,uint8_t percent);
-void act_SetMMX(float thrust,float yaw,float pitch,float roll);
-
-
+void act_SetMotorSpeed(uint8_t idx, uint16_t percent);
+void act_SetMMX(float thrust, float yaw, float pitch, float roll);
 #endif // ACT_H
--- FC_firmware/Src/act/impl/act.c
+++ FC_firmware/Src/act/impl/act.c
@@ -1,117 +1,108 @@
 #include "act/act.h"
 #include "log/log.h"
-#include "act.h"
-#include "math.h"
 
+#include "arm_math.h"
 
-act_Motor motor_1;
-act_Motor motor_2;
-act_Motor motor_3;
-act_Motor motor_4;
+typedef struct {
+    TIM_HandleTypeDef* timer;
+    uint32_t channel;
+} act_Motor;
 
+static act_Motor act_Motors[4];
 
+static void act_InitMotor(act_Motor* motor, TIM_HandleTypeDef* timer, uint32_t channel);
+static void act_InitPwm(TIM_HandleTypeDef* timer, uint32_t channel);
 
-void act_Init(TIM_HandleTypeDef * tim1,uint32_t ch1,TIM_HandleTypeDef * tim2,uint32_t ch2,TIM_HandleTypeDef * tim3,uint32_t ch3,TIM_HandleTypeDef * tim4,uint32_t ch4)
-{
+void act_Init(TIM_HandleTypeDef* timers[act_MOTOR_COUNT], uint32_t* channels[act_MOTOR_COUNT]) {
+    // log_Debug("Initalizing act...");
 
-	//log_Debug("Initalizing act...");
-
-    act_CreateMotor(&motor_1, tim1, ch1, 1);
-    act_CreateMotor(&motor_2, tim2, ch2, 2);
-    act_CreateMotor(&motor_3, tim3, ch3, 3);
-    act_CreateMotor(&motor_4, tim4, ch4, 4);
+    for (uint i = 0; i < act_MOTOR_COUNT; i++) {
+        act_InitMotor(&act_Motors[i], timers[i], channels[i]);
+    }
 }
 
-void act_createMotor(act_Motor* motor,TIM_HandleTypeDef * timer,uint32_t channel, uint8_t number)
-{
+static void act_InitMotor(act_Motor* motor, TIM_HandleTypeDef* timer, uint32_t channel) {
+    motor->channel = channel;
+    motor->timer = timer;
 
-	motor->timer=timer;
-	motor->channel = channel;
-	motor->number=number;
+    act_InitPwm(timer, channel);
 
-	act_PwmInit(timer, channel);
-
-	HAL_TIM_PWM_Start(motor->timer, motor->channel);
-
-	__HAL_TIM_SET_COMPARE(motor->timer, motor->channel,PWM_min + (0 * PWM_range / 100));
-
+    __HAL_TIM_SET_COMPARE(motor->timer, motor->channel, act_PWM_MIN + (0 * act_PWM_RANGE / 100));
+    HAL_TIM_PWM_Start(motor->timer, motor->channel);
 }
 
-void act_PwmInit(TIM_HandleTypeDef * timer, uint32_t channel)
-{
-	  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-	  TIM_MasterConfigTypeDef sMasterConfig = {0};
-	  TIM_OC_InitTypeDef sConfigOC = {0};
+static void act_InitPwm(TIM_HandleTypeDef* timer, uint32_t channel) {
 
-	  //timer->Instance = timer;
-	  timer->Init.Prescaler = 32-1;  //32 MHz clock
-	  timer->Init.CounterMode = TIM_COUNTERMODE_UP;
-	  timer->Init.Period = 20000;
-	  timer->Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-	  timer->Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-	  if (HAL_TIM_Base_Init(timer) != HAL_OK)
-	  {
-	    Error_Handler();
-	  }
-	  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-	  if (HAL_TIM_ConfigClockSource(timer, &sClockSourceConfig) != HAL_OK)
-	  {
-	    Error_Handler();
-	  }
-	  if (HAL_TIM_PWM_Init(timer) != HAL_OK)
-	  {
-	    Error_Handler();
-	  }
-	  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-	  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-	  if (HAL_TIMEx_MasterConfigSynchronization(timer, &sMasterConfig) != HAL_OK)
-	  {
-	    Error_Handler();
-	  }
-	  sConfigOC.OCMode = TIM_OCMODE_PWM1;
-	  sConfigOC.Pulse = 0;
-	  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-	  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-	  if (HAL_TIM_PWM_ConfigChannel(timer, &sConfigOC, channel) != HAL_OK)
-	  {
-	    Error_Handler();
-	  }
-	  HAL_TIM_MspPostInit(timer);
+    TIM_ClockConfigTypeDef sClockSourceConfig = { 0 };
+    TIM_MasterConfigTypeDef sMasterConfig = { 0 };
+    TIM_OC_InitTypeDef sConfigOC = { 0 };
 
+    // timer->Instance = timer;
+    timer->Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+    timer->Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    timer->Init.CounterMode = TIM_COUNTERMODE_UP;
+    timer->Init.Period = act_PWM_RANGE * 10;
+    timer->Init.Prescaler = 32 - 1; // 32 MHz clock
+
+    if (HAL_TIM_Base_Init(timer) != HAL_OK) {
+        Error_Handler();
+    }
+
+    sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+    if (HAL_TIM_ConfigClockSource(timer, &sClockSourceConfig) != HAL_OK) {
+        Error_Handler();
+    }
+
+    if (HAL_TIM_PWM_Init(timer) != HAL_OK) {
+        Error_Handler();
+    }
+
+    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+    sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+
+    if (HAL_TIMEx_MasterConfigSynchronization(timer, &sMasterConfig) != HAL_OK) {
+        Error_Handler();
+    }
+
+    sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+    sConfigOC.OCMode = TIM_OCMODE_PWM1;
+    sConfigOC.Pulse = 0;
+
+    if (HAL_TIM_PWM_ConfigChannel(timer, &sConfigOC, channel) != HAL_OK) {
+        Error_Handler();
+    }
+
+    HAL_TIM_MspPostInit(timer);
 }
 
-void act_setMotorSpeed( uint8_t number ,uint8_t percent)
-{
-	act_Motor motor;
+void act_SetMotorSpeed(uint8_t idx, uint16_t percent) {
+    assert(idx < act_MOTOR_COUNT);
 
-	if (number == 1) {
-	    motor = motor_1;
-	} else if (number == 2) {
-	    motor = motor_2;
-	} else if (number == 3) {
-	    motor = motor_3;
-	}else if (number == 4) {
-	    motor = motor_4;
-	} else {
-		//log_Debug("not valid motor...");
-	    return;
-	}
+    act_Motor motor = act_Motors[idx];
+    __HAL_TIM_SET_COMPARE(motor.timer, motor.channel, act_PWM_MIN + (percent * act_PWM_RANGE / 100));
 
-	 __HAL_TIM_SET_COMPARE(motor.timer, motor.channel,PWM_min + (percent * PWM_range / 100));
-	 //log_Debug("beallitva %d. motor\r\n", motor.number);
+    // log_Debug("beallitva %d. motor\r\n", motor.number);
 }
 
-void act_setMMX(float thrust,float yaw,float pitch,float roll)
-{
-	uint32_t speed_1= fmin(1000,(thrust + yaw + pitch + roll)*1000);
-	uint32_t speed_2= fmin(1000,(thrust - yaw + pitch - roll)*1000);
-	uint32_t speed_3= fmin(1000,(thrust + yaw - pitch + roll)*1000);
-	uint32_t speed_4= fmin(1000,(thrust - yaw - pitch - roll)*1000);
+void act_SetMMX(float thrust, float yaw, float pitch, float roll) {
+    assert(act_MOTOR_COUNT == 4);
 
-	//log_Debug("1 :%d 2 :%d 3 :%d 4 :%d\r\n",speed_1,speed_2,speed_3,speed_4);
+    float speed[act_MOTOR_COUNT] = {
+        thrust + yaw + pitch + roll, //
+        thrust - yaw + pitch - roll, //
+        thrust + yaw - pitch + roll, //
+        thrust - yaw - pitch - roll  //
+    };
 
-	act_setMotorSpeed( 1,speed_1);
-	act_setMotorSpeed( 2,speed_2);
-	act_setMotorSpeed( 3,speed_3);
-	act_setMotorSpeed( 4,speed_4);
+    float tmp[act_MOTOR_COUNT];
+
+    arm_scale_f32(speed, act_PWM_RANGE, tmp, act_MOTOR_COUNT);
+    arm_clip_f32(tmp, speed, act_PWM_MIN, act_PWM_MAX, act_MOTOR_COUNT);
+
+    for (int i = 0; i < act_MOTOR_COUNT; i++) {
+        act_setMotorSpeed(i, speed[i]);
+    }
+
+    // log_Debug("1:%d 2:%d 3:%d 4:%d\n", speed[0], speed[1], speed[2], speed[3]);
 }

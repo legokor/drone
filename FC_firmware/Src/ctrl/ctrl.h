97090136ad168f30e4ff764fb59072a5ef5f4da7
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2025-05-15 16:33:23 +0200

    Add modes

--- FC_firmware/Src/ctrl/ctrl.h
+++ FC_firmware/Src/ctrl/ctrl.h
@@ -1,8 +1,19 @@
 #ifndef CTRL_H
 #define CTRL_H
 
+typedef enum ctrl_Mode {
+    ctrl_Idle,
+    ctrl_RC,
 
+    //  (normal / abort)
+    ctrl_SoftLanding,
+    ctrl_GPS_Waypoint
+
+} ctrl_Mode;
 
 void ctrl_Init(void);
+
+void ctrl_SetMode(ctrl_Mode mode);
+ctrl_Mode ctrl_GetMode(void);
 
 #endif // CTRL_H
--- FC_firmware/Src/ctrl/impl/ctrl.c
+++ FC_firmware/Src/ctrl/impl/ctrl.c
@@ -1,6 +1,16 @@
 #include "ctrl/ctrl.h"
 #include "log/log.h"
 
+static ctrl_Mode _ctrl_CurrentMode;
+
 void ctrl_Init(void) {
     log_Debug("Initializing ctrl...");
+}
+
+void ctrl_SetMode(ctrl_Mode mode) {
+    _ctrl_CurrentMode = mode;
+}
+
+ctrl_Mode ctrl_GetMode(void) {
+    return _ctrl_CurrentMode;
 }

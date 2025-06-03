3bfb7d5fe1b02c5aa4d9fd848374676a9bf01ec8
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2025-06-03 17:36:58 +0200

    Make _act_armed volatile
    Also refactored a bit.

--- FC_firmware/Src/act/impl/act.c
+++ FC_firmware/Src/act/impl/act.c
@@ -4,37 +4,36 @@
 #include <stdbool.h>
 #include "arm_math.h"
 
-static bool act_Armed = false;
+#include <stdbool.h>
 
-typedef struct {
+static volatile bool _act_armed = false;
+
+typedef struct act_Motor {
     TIM_HandleTypeDef* timer;
     uint32_t channel;
 } act_Motor;
 
-static act_Motor act_Motors[4];
-
-static void act_initMotor(act_Motor* motor, TIM_HandleTypeDef* timer, uint32_t channel);
+static act_Motor _act_motors[4] = { 0 };
 
 void act_init(TIM_HandleTypeDef* timers[act_MOTOR_COUNT], uint32_t channels[act_MOTOR_COUNT]) {
     log_debug("Initializing act...");
 
-    for (uint8_t i = 0; i < act_MOTOR_COUNT; i++)
-        act_initMotor(&act_Motors[i], timers[i], channels[i]);
-}
+    for (uint8_t i = 0; i < act_MOTOR_COUNT; i++) {
+        TIM_HandleTypeDef* timer = timers[i];
+        uint32_t channel = channels[i];
 
-static void act_initMotor(act_Motor* motor, TIM_HandleTypeDef* timer, uint32_t channel) {
-    motor->channel = channel;
-    motor->timer = timer;
+        _act_motors[i] = (act_Motor) { .channel = channel, .timer = timer };
 
-    __HAL_TIM_SET_COMPARE(motor->timer, motor->channel, act_PWM_MIN + (0 * act_PWM_RANGE / 100));
-    HAL_TIM_PWM_Start(motor->timer, motor->channel);
+        __HAL_TIM_SET_COMPARE(timer, channel, act_PWM_MIN + (0 * act_PWM_RANGE / 100));
+        HAL_TIM_PWM_Start(timer, channel);
+    }
 }
 
 static void act_setMotorSpeed(uint8_t idx, uint16_t percent) {
     assert_param(idx < act_MOTOR_COUNT);
 
-    act_Motor motor = act_Motors[idx];
-    __HAL_TIM_SET_COMPARE(motor.timer, motor.channel, act_PWM_MIN + (percent * act_PWM_RANGE / 100));
+    act_Motor m = _act_motors[idx];
+    __HAL_TIM_SET_COMPARE(m.timer, m.channel, act_PWM_MIN + (percent * act_PWM_RANGE / 100));
 
     // log_debug("beallitva %d. motor\r\n", motor.number);
 }
@@ -42,7 +41,7 @@ static void act_setMotorSpeed(uint8_t idx, uint16_t percent) {
 void act_setMMX(float thrust, float yaw, float pitch, float roll) {
     assert_param(act_MOTOR_COUNT == 4);
 
-    if (!act_Armed) {
+    if (!_act_armed) {
         act_disarm();
         return;
     }
@@ -56,7 +55,9 @@ void act_setMMX(float thrust, float yaw, float pitch, float roll) {
 
     float tmp[act_MOTOR_COUNT];
 
+    // scale from speed to tmp
     arm_scale_f32(speed, act_PWM_RANGE, tmp, act_MOTOR_COUNT);
+    // clip from tmp to speed
     arm_clip_f32(tmp, speed, act_PWM_MIN, act_PWM_MAX, act_MOTOR_COUNT);
 
     for (int i = 0; i < act_MOTOR_COUNT; i++)
@@ -68,13 +69,14 @@ void act_setMMX(float thrust, float yaw, float pitch, float roll) {
 void act_arm(void) {
     assert_param(act_Armed == false);
 
-    act_Armed = true;
+    _act_armed = true;
 }
 
 void act_disarm(void) {
     for (int i = 0; i < act_MOTOR_COUNT; i++)
         act_setMotorSpeed(i, 0);
 
-    assert_param(act_Armed == false);
-    act_Armed = false;
+    // we assert later, to make sure that the motors get turned off no matter what
+    bool was_armed = _act_armed;
+    _act_armed = false;
 }

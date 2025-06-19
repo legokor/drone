ce66c2c2aad4c9d11f4441a93b3d44837c709124
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2025-06-19 13:59:13 +0200

    Refactor log.c

--- FC_firmware/Src/log/impl/log.c
+++ FC_firmware/Src/log/impl/log.c
@@ -11,71 +11,63 @@
 
 void log_init(void) {}
 
-#define _log_WRITE_BUF_SIZE 256
+#define _log_BUF_SIZE 256
 
-static char _log_buf[_log_WRITE_BUF_SIZE];
+#define _log_TYPE_SIZE ((uint8_t) 3)
+#define _log_PREFIX_SIZE (_log_TYPE_SIZE + sizeof("[] ") - 1)
 
-static void _log_write(log_LogLevel level, const char* format, va_list args) {
-    const char LEVEL_STR[5][3] = { "xxx", "DBG", "INF", "WRN", "ERR" };
+static void _log_write(log_LogLevel level, const char* restrict format, va_list args) {
+    const char LEVEL_STR[5][_log_TYPE_SIZE] = { "xxx", "DBG", "INF", "WRN", "ERR" };
+    static char _log_buf[_log_BUF_SIZE];
 
     if (level != log_NONE) {
         _log_buf[0] = '[';
-        strncpy(_log_buf + 1, LEVEL_STR[level], 3);
-        _log_buf[4] = ']';
-    }
+        memcpy(_log_buf + 1, LEVEL_STR[level], _log_TYPE_SIZE);
+        _log_buf[_log_TYPE_SIZE + 1] = ']';
+        _log_buf[_log_TYPE_SIZE + 2] = ' ';
 
-    if (level == log_NONE) {
-        vsnprintf(_log_buf, _log_WRITE_BUF_SIZE, format, args);
+        int cnt = vsnprintf(_log_buf + _log_PREFIX_SIZE, _log_BUF_SIZE - (_log_PREFIX_SIZE + 2), format, args);
+        if (cnt < 0) {
+            err_tryIgnorable(cnt >= 0, "Couldn't format log");
+        }
+
+        int idx = (uint8_t) cnt < _log_BUF_SIZE - (_log_PREFIX_SIZE + 2) ? cnt + _log_PREFIX_SIZE : _log_BUF_SIZE - 3;
+
+        // copy null too
+        memcpy(_log_buf + idx, "\r\n", 3);
     } else {
-        int ret = vsnprintf(_log_buf + 5, _log_WRITE_BUF_SIZE - 7, format, args);
-        int nullTermIndex = ret < _log_WRITE_BUF_SIZE - 7 ? ret + 5 : _log_WRITE_BUF_SIZE - 3;
-
-        _log_buf[nullTermIndex] = '\r';
-        _log_buf[nullTermIndex + 1] = '\n';
-        _log_buf[nullTermIndex + 2] = '\0';
+        err_tryIgnorable(vsnprintf(_log_buf, _log_BUF_SIZE, format, args) >= 0, "Couldn't format log");
     }
 
     tel_writeString(LOG_TOPIC, _log_buf);
 }
 
-void log_raw(const char* format, ...) {
-    va_list args;
-    va_start(args, format);
-    _log_write(log_NONE, format, args);
+#define _log_LOG_VARIANT(var)      \
+    va_list args;                  \
+    va_start(args, format);        \
+    _log_write(var, format, args); \
     va_end(args);
+
+void log_raw(const char* restrict format, ...) {
+    _log_LOG_VARIANT(log_NONE);
 }
 
-void log_debug(const char* format, ...) {
-    va_list args;
-    va_start(args, format);
-    _log_write(log_DEBUG, format, args);
-    va_end(args);
+void log_debug(const char* restrict format, ...) {
+    _log_LOG_VARIANT(log_DEBUG);
 }
 
-void log_info(const char* format, ...) {
-    va_list args;
-    va_start(args, format);
-    _log_write(log_INFO, format, args);
-    va_end(args);
+void log_info(const char* restrict format, ...) {
+    _log_LOG_VARIANT(log_INFO);
 }
 
-void log_warn(const char* format, ...) {
-    va_list args;
-    va_start(args, format);
-    _log_write(log_WARN, format, args);
-    va_end(args);
+void log_warn(const char* restrict format, ...) {
+    _log_LOG_VARIANT(log_WARN);
 }
 
-void log_error(const char* format, ...) {
-    va_list args;
-    va_start(args, format);
-    _log_write(log_ERROR, format, args);
-    va_end(args);
+void log_error(const char* restrict format, ...) {
+    _log_LOG_VARIANT(log_ERROR);
 }
 
-void log_write(log_LogLevel level, const char* format, ...) {
-    va_list args;
-    va_start(args, format);
-    _log_write(level, format, args);
-    va_end(args);
+void log_write(log_LogLevel level, const char* restrict format, ...) {
+    _log_LOG_VARIANT(level);
 }

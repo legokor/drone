40aaedc85feee274f86bc3e6676de3301b0eb0fd
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2025-09-19 10:37:23 +0200

    Sketch out guide

--- FC_firmware/Src/guide/guide.h
+++ FC_firmware/Src/guide/guide.h
@@ -1,6 +1,11 @@
 #ifndef GUIDE_H
 #define GUIDE_H
 
+#include "ctrl/ctrl.h"
+#include "llc/llc.h"
+
 void guide_init(void);
+
+llc_ThrustVec guide_get_ref(ctrl_Mode mode);
 
 #endif // GUIDE_H
--- FC_firmware/Src/guide/impl/guide.c
+++ FC_firmware/Src/guide/impl/guide.c
@@ -1,6 +1,18 @@
 #include "guide/guide.h"
+#include "err/err.h"
 #include "log/log.h"
 
 void guide_init(void) {
     log_debug("Initializing guide...");
+}
+
+llc_ThrustVec guide_get_ref(ctrl_Mode mode) {
+    switch (mode) {
+        case ctrl_Idle: return (llc_ThrustVec) { 0 };
+
+        case ctrl_RC: break;
+
+        case ctrl_SoftLanding: err_todo(); break;
+        case ctrl_GPS_Waypoint: err_todo(); break;
+    }
 }

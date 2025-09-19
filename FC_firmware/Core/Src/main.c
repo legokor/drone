fc8609f8c3d1c78acabacf82d64189c6dc1013cd
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2025-09-19 08:16:44 +0200

    Add interrup handlers to Core

--- FC_firmware/Core/Src/main.c
+++ FC_firmware/Core/Src/main.c
@@ -212,3 +212,41 @@ void assert_failed(uint8_t* file, uint32_t line) {
     /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
+
+#include "irq/impl/irq_list.h"
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart) {
+    void* tmp = (void*) huart;
+    _irq_triggerCbs(irq_UART_RX_CPLT, tmp);
+}
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef* huart) {
+    void* tmp = (void*) huart;
+    _irq_triggerCbs(irq_UART_TX_CPLT, tmp);
+}
+
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef* huart, uint16_t pos) {
+    UNUSED(pos);
+    void* tmp = (void*) huart;
+    _irq_triggerCbs(irq_UART_RX_EVENT, tmp);
+}
+
+void HAL_UART_ErrorCallback(UART_HandleTypeDef* huart) {
+    void* tmp = (void*) huart;
+    _irq_triggerCbs(irq_UART_ERROR, tmp);
+}
+
+void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef* hspi) {
+    void* tmp = (void*) hspi;
+    _irq_triggerCbs(irq_SPI_TX_CPLT, tmp);
+}
+
+void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef* hspi) {
+    void* tmp = (void*) hspi;
+    _irq_triggerCbs(irq_SPI_RX_CPLT, tmp);
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim) {
+    void* tmp = (void*) htim;
+    _irq_triggerCbs(irq_TIM_PERIOD_ELAPSED, tmp);
+}

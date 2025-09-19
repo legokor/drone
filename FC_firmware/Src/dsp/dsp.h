c123ebeca9612efd36f6a10d292bbc3a7bb0f4f6
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2025-09-19 10:40:18 +0200

    Add basic dsp

--- FC_firmware/Src/dsp/dsp.h
+++ FC_firmware/Src/dsp/dsp.h
@@ -1,6 +1,39 @@
 #ifndef DSP_H
 #define DSP_H
 
+#include "imu/imu.h"
+
+// TODO: simplify...
+
+/// some X macro magic to define inputs / outputs for the module
+
+// the declaration for the data item
+#define _dsp_DATA_IMPL(type, name)          \
+    [[nodiscard]] type dsp_get##name(void); \
+    void dsp_set##name(type name);
+
+// this will get expanded to either the declaration (dsp.h)
+// or the definition (dsp.c)
+#define _dsp_DATA_ITEM(type, name) _dsp_DATA_IMPL(type, name)
+
+// we define the actual input / output values
+#define _dsp_DATA                         \
+    /* input */                           \
+    _dsp_DATA_ITEM(imu_Vec3, InAcc) /**/  \
+    _dsp_DATA_ITEM(imu_Vec3, InGyr) /**/  \
+    _dsp_DATA_ITEM(float, InBar)    /**/  \
+                                          \
+    /* output */                          \
+    _dsp_DATA_ITEM(imu_Vec3, OutAng) /**/ \
+    _dsp_DATA_ITEM(imu_Vec3, OutVel) /**/ \
+    _dsp_DATA_ITEM(float, OutHeight)
+
+_dsp_DATA
+
+// undef the impl, so we can redefine it later
+#undef _dsp_DATA_IMPL
+
 void dsp_init(void);
+void dsp_update(void);
 
 #endif // DSP_H
--- FC_firmware/Src/dsp/impl/dsp.c
+++ FC_firmware/Src/dsp/impl/dsp.c
@@ -1,6 +1,61 @@
 #include "dsp/dsp.h"
+#include "imu/imu.h"
 #include "log/log.h"
+
+#include "arm_math.h"
+#include "stm32f4xx_hal.h"
+
+#include <math.h>
+#include <stdio.h>
+
+// the definitions for the inputs / outputs
+#define _dsp_DATA_IMPL(type, name)  \
+    static type _dsp_##name;        \
+    type dsp_get##name(void) {      \
+        return _dsp_##name;         \
+    }                               \
+                                    \
+    void dsp_set##name(type name) { \
+        _dsp_##name = name;         \
+    }
+
+_dsp_DATA
+
+#undef _dsp_DATA_IMPL
+
+static uint32_t _dsp_lastRun = 0;
+
+// static imu_Vec3 _dsp_angleGyr = { 0 };
+
+static float _dsp_alpha = 0.97;
 
 void dsp_init(void) {
     log_debug("Initializing dsp...");
+
+    _dsp_lastRun = HAL_GetTick();
+}
+
+void dsp_update(void) {
+    // TODO: use a timer for usec resolution
+    uint32_t now = HAL_GetTick();
+    float dt = 0.001 * (now - _dsp_lastRun);
+    _dsp_lastRun = now;
+
+    imu_Vec3 inAcc = dsp_getInAcc();
+    imu_Vec3 inGyr = dsp_getInGyr();
+
+    static imu_Vec3 _dsp_gyrIntegral = { 0 };
+    _dsp_gyrIntegral.roll += inGyr.roll * dt;
+    _dsp_gyrIntegral.pitch += inGyr.pitch * dt;
+    _dsp_gyrIntegral.yaw += inGyr.yaw * dt;
+
+    float rollA = atan2f(inAcc.x, inAcc.z);
+    float pitchA = atan2f(inAcc.y, inAcc.z);
+
+    static float _dps_rollComp = 0, _dsp_pitchComp = 0;
+
+    _dps_rollComp = rollA * (1 - _dsp_alpha) + _dsp_alpha * (_dps_rollComp + inGyr.y * dt);
+    _dsp_pitchComp = pitchA * (1 - _dsp_alpha) + _dsp_alpha * (_dsp_pitchComp + inGyr.x * dt);
+
+    dsp_setOutAng((imu_Vec3) { .roll = _dps_rollComp, .pitch = _dsp_pitchComp, .yaw = _dsp_gyrIntegral.yaw });
 }

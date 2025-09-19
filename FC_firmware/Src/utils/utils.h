7c12098fad046a0b0c02f6b89cfb2b5e51179e3c
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2025-09-19 08:53:46 +0200

    Add some utils

--- FC_firmware/Src/utils/utils.h
+++ FC_firmware/Src/utils/utils.h
@@ -4,4 +4,9 @@
 #define utils_QUOTE(str) #str
 #define utils_EXPAND_AND_QUOTE(str) utils_QUOTE(str)
 
+#define utils_PI 3.14159265358979323846f
+
+#define utils_MIN(a, b) ((a) < (b) ? (a) : (b))
+#define utils_MAX(a, b) ((a) > (b) ? (a) : (b))
+
 #endif // UTILS_H
